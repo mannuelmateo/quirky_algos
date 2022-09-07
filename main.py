af5f48cf87fa7592cd8f7aca895087b9fce10125
manuel <mmateo@loyola.edu>
2022-09-06 20:28:21 -0400

    adds both bogosort and stalinsort

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+from typing import Callable
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def bogo_sort(l, iteration=1):
+    print(f"{iteration=}")
+    for x in range(len(l)):
+        if x < len(l) - 1:
+            if not (l[x] <= l[x + 1]):
+                random.shuffle(l)
+                return bogo_sort(l, iteration + 1)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def stalin_sort(l):
+    current = l[0]
+    sorted_l = []
+    for x in l:
+        if x >= current:
+            sorted_l.append(x)
+            current = x
+    return sorted_l
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def test_sort(sorting_func: Callable, element_count=10, min_val=1, max_val=20):
+    test_list = [random.randint(min_val, max_val) for x in range(element_count)]
+
+    print(test_list)
+
+    list_copy = list(test_list)  # shallow copy
+
+    try:
+        sorted_list = sorting_func(list_copy)
+        if not sorted_list is None:
+            list_copy = sorted_list
+        test_list.sort()
+
+        print(list_copy, test_list)
+    except Exception as e:
+        print(e)
+
+
+def main():
+    test_sort(bogo_sort, element_count=5)
+
+
+if __name__ == "__main__":
+    main()
